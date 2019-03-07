16f626906d73d3f3a99f82a32f068f2ae24f32e3
Rahul Sindhav <rahulkumarsindhav@gmail.com>
2019-03-07 09:42:06 +0530

    First commit

---
+++ WeatherServiceApplication/IWeatherService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WeatherServiceApplication
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract(SessionMode = SessionMode.Required, CallbackContract = typeof(ICallBackService))]
+    public interface IWeatherService
+    {
+        [OperationContract(IsOneWay = false, IsInitiating = true)]
+        string Subscribe();
+        [OperationContract(IsOneWay = false, IsInitiating = true)]
+        string Unsubscribe();
+        [OperationContract(IsOneWay = true)]
+        void Broadcast(Message message);
+    }
+    public interface ICallBackService
+    {
+        [OperationContract(IsOneWay = true)]
+        void Update(Message message);
+    }
+    [MessageContract]
+    public class Message
+    {
+        private string place;
+        private string time;
+        private string temp;
+        private string wind_speed;
+        private string humidity;
+
+        [MessageBodyMember]
+        public string Place
+        {
+            get { return this.place; }
+            set { this.place = value; }
+        }
+        [MessageBodyMember]
+        public string Time
+        {
+            get { return this.time; }
+            set { this.time = value; }
+        }
+        [MessageBodyMember]
+        public string Temp
+        {
+            get { return this.temp; }
+            set { this.temp = value; }
+        }
+        [MessageBodyMember]
+        public string WindSpeed
+        {
+            get { return this.wind_speed; }
+            set { this.wind_speed = value; }
+        }
+        [MessageBodyMember]
+        public string Humidity
+        {
+            get { return this.humidity; }
+            set { this.humidity = value; }
+        }
+
+    }
+}
---
+++ WeatherServiceApplication/WeatherService.svc.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WeatherServiceApplication
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class WeatherService : IWeatherService
+    {
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Rahul\source\repos\WeatherServiceApplication\WeatherServiceApplication\App_Data\WeatherDatabase.mdf;Integrated Security=True";
+        ICallBackService icbs = null;
+        private static List<ICallBackService> subscribers = new List<ICallBackService>();
+        public void Broadcast(Message message)
+        {
+            foreach (ICallBackService subscriber in subscribers)
+            {
+                try
+                {
+                    SqlConnection connection = new SqlConnection(connectionString);
+                    SqlCommand cmd = new SqlCommand();
+                    connection.Open();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "insert into Weather(place,time,temp,wind_speed,humidity) values (@place,@time,@temp,@windspeed,@humidity)";
+                    cmd.Parameters.AddWithValue("@place", message.Place);
+                    cmd.Parameters.AddWithValue("@time", message.Time);
+                    cmd.Parameters.AddWithValue("@temp", Decimal.Parse(message.Temp));
+                    cmd.Parameters.AddWithValue("@windspeed", Decimal.Parse(message.WindSpeed));
+                    cmd.Parameters.AddWithValue("@humidity", Decimal.Parse(message.Humidity));
+                    int i = cmd.ExecuteNonQuery();
+                    if (((IChannel)subscriber).State == CommunicationState.Opened)
+                        subscriber.Update(message);
+                    else
+                        subscribers.Remove(subscriber);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return;
+        }
+
+        public string Subscribe()
+        {
+            try
+            {
+                icbs = OperationContext.Current.GetCallbackChannel<ICallBackService>();
+                SqlConnection connection = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand();
+                connection.Open();
+                cmd.Connection = connection;
+                cmd.CommandText = "INSERT INTO Subscribers(subscriber) VALUES (@object)";
+                cmd.Parameters.AddWithValue("@object", icbs);
+                subscribers.Add(icbs);
+                return "Successfully subscribed for weather updates!";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return "Something went wrong! Please try after some time.";
+            }
+
+
+        }
+
+        public string Unsubscribe()
+        {
+            try
+            {
+                icbs = OperationContext.Current.GetCallbackChannel<ICallBackService>();
+                lock (subscribers)
+                {
+                    subscribers.Remove(icbs);
+                }
+                return "Successfully unsubscribed!";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return "Something went wrong! Try after some time.";
+            }
+        }
+    }
+}
